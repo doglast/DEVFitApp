41ceb8ec47d3ac68cfbb8ceb67635af5ddd67ef3
Doglast <des212425@gmail.com>
2024-11-26 22:51:26 -0300

    feat: Ajuste para consumir API no processo de login

--- app/login/index.jsx
+++ app/login/index.jsx
@@ -1,44 +1,87 @@
-import { View, Text, Image, Pressable, TextInput, StyleSheet, Platform } from 'react-native';
-import React from 'react';
+// app/login/index.jsx
+import React, { useState } from 'react';
+import { 
+  View, 
+  Text, 
+  Image, 
+  TextInput, StyleSheet, Pressable, Alert, Platform } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 import Colors from './../../constants/Colors';
-import { Link } from "expo-router";
 
 const LoginScreen = () => {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('https://www.rodrigozambon.com.br/devfitness/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const token = data.token;
+        console.log('Login successful:', token);
+        await AsyncStorage.setItem('userToken', token);
+
+        // Navigate to the homepage
+        navigation.navigate('HomePage');
+      } else {
+        Alert.alert('Erro ao entrar', 'E-mail ou senha inválida.');
+      }
+    } catch (error) {
+      console.error('Error logging in:', error);
+      Alert.alert('Error', 'Um erro ocorreu durante o login.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Image source={require('./../../assets/images/dev-fitness-icon.png')}
-        style={styles.icon}
+      <Image 
+        source={require('./../../assets/images/dev-fitness-icon.png')} 
+        style={styles.icon} 
       />
       <View style={styles.page}>
         <Text style={styles.textTitle}>
           Gerencie o fluxo de pagamento dos seus alunos de qualquer lugar!
         </Text> 
 
-        <Text style={[styles.textLabel, {marginTop:40}]}>Email</Text>
+        <Text style={[styles.textLabel, { marginTop: 40 }]}>Email</Text>
         <View style={styles.action}>
           <TextInput
             style={styles.textInput}
             autoCapitalize='none'
             keyboardType='email-address'
+            value={email}
+            onChangeText={setEmail}
+            placeholder='Enter your email'
           />
         </View>
-        <Text style={[styles.textLabel, {marginTop:30}]}>Senha</Text>
+        <Text style={[styles.textLabel, { marginTop: 30 }]}>Senha</Text>
         <View style={styles.action}>
           <TextInput
-            style={[styles.textInput, {color:Colors.black}]}
+            style={[styles.textInput, { color: Colors.black }]}
             autoCapitalize='none'
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+            placeholder='Enter your password'
           />
         </View>       
-        
-        <Pressable style={styles.pressable}>
-          <Link style={styles.link} href={'/homepage'}>
-            <Text style={styles.textSign}>Entrar</Text>
-          </Link>
+
+        <Pressable style={styles.pressable} onPress={handleLogin}>
+          <Text style={styles.textSign}>Entrar</Text>
         </Pressable>
       </View>
     </View>
-  )
-}
+  );
+};
 
 export default LoginScreen;
 
