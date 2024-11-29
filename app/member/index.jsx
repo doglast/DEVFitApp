17aeaa2b731dc3a075dddd9aed30f61c45d7fd3c
Doglast <des212425@gmail.com>
2024-11-29 20:43:30 -0300

    fix: remoção do nome do header

--- app/member/index.jsx
+++ app/member/index.jsx
@@ -135,7 +135,6 @@ const MemberScreen = ({ route }) => {
             source={require('../../assets/icons/left_arrow_icon.png')}
           />
         </TouchableOpacity>
-        <Text style={styles.headerText}>{memberData.name || 'N/A'}</Text>
       </View>
 
       <MemberViewItem item={memberData} />
